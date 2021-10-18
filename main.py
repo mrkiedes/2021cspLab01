b51dd6a9f5f9a4ccc1a891372eec0375893ba2f5
Mr. Kiedes <akiedes@vtsd.com>
2021-10-18 11:46:13 -0400

    Update Base Files

--- main.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Lab's work goes in the main function")
-
-if __name__ == "__main__":
-    main()
---
+++ mainFile.py
@@ -0,0 +1,30 @@
+import turtle as trtl
+
+def main():
+    # Create the Turtle
+    gary = trtl.Turtle()
+
+    # Setup the Box
+    box(gary)
+
+    #Drawing the Lines
+
+
+    # Keep Screen Active
+    wn = trtl.Screen()
+    wn.mainloop()
+
+def box(gary):
+    xPos = -500
+    yPos = -300
+    gary.speed(0)
+    gary.penup()
+    gary.goto(xPos, yPos)
+    gary.pendown()
+    gary.goto(xPos+980, yPos)
+    gary.goto(xPos+980, yPos+630)
+    gary.goto(xPos, yPos+630)
+    gary.goto(xPos, yPos)
+
+if __name__ == "__main__":
+    main()
