95ea752994a7afef73ab6833011ff52d5d6b8647
vjosaukella <vjosa.ukella@vtsd.com>
2021-10-20 12:34:14 -0400

    woo

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import turtle as trtl
+
 def main():
-    print("Lab's work goes in the main function")
+    #create the Turtle
+    gary = trtl.Turtle()
+    #set up the box
+    box(gary)
+    #Drawing the lines
+
+    # Keep Screen Active
+    wn = trtl.Screen()
+    wn.mainloop()
+
+def box(gary):
+    xPos = -500
+    yPos = -300
+    gary.speed(0)
+    gary.penup()
+    gary.goto(xPos, yPos)
+    gary.pendown()
+    gary.goto(xPos+980, yPos)
+    gary.goto(xPos + 980, yPos+630)
+    gary.goto(xPos, yPos+630)
+    gary.goto(xPos, yPos)
+
+
 
 if __name__ == "__main__":
     main()
